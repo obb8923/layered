4daf0241eaa7393bb1bc7390c93fe4bb03f1a944
obb8923 <obb8923@naver.com>
2025-06-26 20:32:31 +0900

    sound contoller

--- src/domain/Home/components/SoundSlider.tsx
+++ src/domain/Home/components/SoundSlider.tsx
@@ -3,6 +3,27 @@ import Slider from "../../../shared/components/Slider";
 import { View } from "react-native";
 import { useEffect, useState } from "react";
 import {Text} from '../../../shared/components/Text'
+import River from '../../../../assets/svgs/River.svg'
+import Frog from '../../../../assets/svgs/Frog.svg'
+import Rain1 from '../../../../assets/svgs/Rain1.svg'
+import Rain2 from '../../../../assets/svgs/Rain2.svg'
+import Bonfire from '../../../../assets/svgs/Bonfire.svg'
+import Bird1 from '../../../../assets/svgs/Bird1.svg'
+import Cricket1 from '../../../../assets/svgs/Cricket1.svg'
+import Wind1 from '../../../../assets/svgs/Wind1.svg'
+import React from 'react';
+import {Colors} from '../../../shared/constants/Colors'
+const iconMapStyle = {width:24,height:24,color:Colors.line}
+const iconMap: Record<string, React.ReactNode> = {
+  rain1: <Rain1 {...iconMapStyle} />, 
+  rain2: <Rain2 {...iconMapStyle} />, 
+  frog1: <Frog {...iconMapStyle} />, 
+  river1: <River {...iconMapStyle} />, 
+  fire1: <Bonfire {...iconMapStyle} />, 
+  bird1: <Bird1 {...iconMapStyle} />,
+  crickets1: <Cricket1 {...iconMapStyle} />,
+  wind1: <Wind1 {...iconMapStyle} />
+};
 
 // 개별 음원 슬라이더 컴포넌트
 export const SoundSlider = ({ label, soundKey, fileName, initialVolume }: {
@@ -24,9 +45,15 @@ export const SoundSlider = ({ label, soundKey, fileName, initialVolume }: {
     return (
       <View 
       className="w-full mb-8"
-      onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}
       >
-        <Text text={label}/>
+        <View style={{flexDirection:'row', alignItems:'center'}}>
+          {/* <Text text={label}/> */}
+        </View>
+        <View className="w-full flex-row items-center">
+        {iconMap[label]}
+        <View 
+          className="ml-4 flex-1 justify-center items-start"
+          onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}>
         <Slider
           min={0}
           max={1}
@@ -36,6 +63,9 @@ export const SoundSlider = ({ label, soundKey, fileName, initialVolume }: {
           value={volume}
           onChange={handleSlider}
         />
+        <Text text={`${Math.floor(volume * 100)}`} type="semibold" className="absolute left-4"/>
+        </View>
+        </View>
       </View>
     );
   }
